4fd868e074541dbef92b963a656cccffabad065e
Daniel Hopper <djhopper95@gmail.com>
2017-03-26 03:00:37 -0400

    remove initial hash merge approach, include interval approach

--- src/double_booked/core.clj
+++ src/double_booked/core.clj
@@ -6,67 +6,20 @@
 (defrecord Event [name start end])
 
 (defn make-event [name start end]
-  (->Event name (/ (c/to-long start) 60000) (/ (c/to-long end) 60000)))
+  (->Event name (/ (c/to-long start) 3600000) (/ (c/to-long end) 3600000)))
 
 (defrecord EventConflict [name1 name2])
 
 (defn make-conflict [event-names]
   (->EventConflict (first event-names) (second event-names)))
 
+(defrecord EventComponent [timestamp type name])
+
+(defn event-to-components [e]
+  [(->EventComponent (get e :start) 'start (get e :name))
+   (->EventComponent (get e :end) 'end (get e :name))])
+
 (def a (Event. "My Birthday" "05-31-95T00:00:00" "05-31-95T23:59:59"))
-
-; to-time-maps
-; this is where we do a list comprehension for each event, creating an entry
-; for each time slot, and convert that into a map.
-(defn to-time-maps [e]
-  (into (hash-map) (let [name   (get e :name)
-                         start  (get e :start)
-                         end    (get e :end)]
-                     (for [time (range start (+ end 1))]
-                       [time [name]]))))
-
-; combinef
-; this is supplied to the fold in get-calendar-map, and simply applies
-; merge-with for each map in the list of maps returned from get-time-maps.
-; could be declared as a lambda with the (fn []()) notation but it was
-; taken out for readability
-(defn combinef ([xs x] (merge-with into x xs)) ([] {}))
-
-; get-calendar-map
-; in this procedure, all the time-maps are merged using merge-with.
-; the heuristic here is that if there is a time conflict, conflicting Events
-; are recorded in a list corresponding to the time stamp in which they overlap
-;
-; NOTE: order may be lost at this point, but it no longer matters, since we
-; have already recorded all conflicts and have the correct date ranges
-; represented in the hash-map.
-(defn get-calendar-map [list-of-events]
-  (reducers/fold combinef (map to-time-maps list-of-events)))
-
-
-; collect-conflicts
-; this procedure goes through the merged calendar map, and collects all of the
-; time slots that have conflicts, and create EventConflict objects out of
-; all of them.
-;
-; it is essentially a one to many map of calendar map entries to conflicts.
-(defn collect-conflicts [list-of-events]
-  (let [conflicting-events  (filter
-                              (fn [x] (> (count (second x)) 1))
-                              (seq (get-calendar-map list-of-events)))
-        conflict-pairs      (map
-                              (fn [x] (map make-conflict (combo/combinations (second x) 2)))
-                              conflicting-events)]
-    (flatten conflict-pairs)))
-
-; get-conflicts
-; this is the only function that needs to be called from the outside
-; it takes a sequence of Events, created using the double-booked.core.Event
-; class, and returns a sequence containing all pairs of conflicting Events
-; by their name.
-;
-(defn get-conflicts [list-of-events]
-  (distinct (collect-conflicts list-of-events)))
 
 ; Define
 
@@ -92,3 +45,26 @@
 (defn get-conflicts-naive [list-of-events]
   (let [event-pairs (combo/combinations list-of-events 2)]
     (filter overlap? event-pairs)))
+
+
+(defn walk-intervals [components active conflicts]
+  (cond
+    (empty? components) conflicts
+    (= 'start (get (first components) :type))
+     (let [next-active (conj active (get (first components) :name))]
+       (walk-intervals (rest components)
+                       next-active
+                       (conj conflicts (map make-conflict (combo/combinations next-active 2)))))
+    (= 'end (get (first components) :type))
+     (let [next-active (disj active  (get (first components) :name))]
+       (walk-intervals (rest components)
+                       next-active
+                       conflicts))))
+
+; get-conflicts-intervals splits the events into their start and end
+; components, and sorts them. once sorted, it returns all events that have
+; starts within other events. looking for nested ends is redundant.
+(defn get-conflicts-intervals [list-of-events]
+  (let [components (map event-to-components list-of-events)
+        sorted-components (sort-by (fn [x] (get x :timestamp)) (flatten components))]
+    (walk-intervals sorted-components (set []) [])))
