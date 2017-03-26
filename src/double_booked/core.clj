9e6dcaa2049fcf5e891b837935b6c45cf7fee907
Daniel Hopper <djhopper95@gmail.com>
2017-03-26 14:39:09 -0400

    wrote a few tests

--- src/double_booked/core.clj
+++ src/double_booked/core.clj
@@ -1,17 +1,12 @@
 (ns double-booked.core
   (:require [clj-time.coerce :as c]
-            [clojure.core.reducers :as reducers]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math.combinatorics :as combo]
+            [clojure.set :as set]))
 
 (defrecord Event [name start end])
 
 (defn make-event [name start end]
   (->Event name (c/to-long start) (c/to-long end)))
-
-(defrecord EventConflict [name1 name2])
-
-(defn make-conflict [event-names]
-  (->EventConflict (first event-names) (second event-names)))
 
 (defrecord EventComponent [timestamp type name])
 
@@ -19,63 +14,62 @@
   [(->EventComponent (get e :start) :start (get e :name))
    (->EventComponent (get e :end) :end (get e :name))])
 
-; Define
-
-(def t1 (make-event "My Birthweek" "1995-05-31" "1995-06-07"))
-(def t2 (make-event "My Birthday" "1995-05-31" "1995-06-01"))
-(def t3 (make-event "First of June" "1995-06-01" "1995-06-02"))
-(def t4 (make-event "Winter Break" "1995-12-22" "1996-01-05"))
-(def t5 (make-event "Christmas" "1995-12-25" "1995-12-26"))
-
-
 ; overlap? is a simple predicate that performs a series of boolean operations
 ; to determine whether or not two events overlap, based off their start
 ; and end timestamps
 (defn overlap? [event-pair]
-  (or  (and (<= (get (first event-pair) :start) (get (second event-pair) :end))
-            (>= (get (first event-pair) :end) (get (second event-pair) :start)))
-       (and (<= (get (second event-pair) :start) (get (first event-pair) :end))
-            (>= (get (second event-pair) :end) (get (first event-pair) :start)))))
+  (or  (and (< (get (first event-pair) :start) (get (second event-pair) :end))
+            (> (get (first event-pair) :end) (get (second event-pair) :start)))
+       (and (< (get (second event-pair) :start) (get (first event-pair) :end))
+            (> (get (second event-pair) :end) (get (first event-pair) :start)))))
 
 
-; get-conflicts-naive is the naive implementation of this problem
+; get-conflicts-naive is the naive implementation of this problem.
 ; essentially, compare each event to all other events, checking for overlaps.
-; this approach is extremely lightweight, but it ends up being slow, due to
-; it's time complexity of O(n^2)
+; using the clojure combinatorics library, generate all possible event pairs,
+; and filter through them using the predicate 'overlap?' defined above.
+; this approach is fairly lightweight code-wise, but it ends up being slow,
+; due to it's time complexity of O(n^2)
 (defn get-conflicts-naive [list-of-events]
   (let [event-pairs (combo/combinations list-of-events 2)]
-    (filter overlap? event-pairs)))
+    (set (map (fn [x] (seq [(get (first x) :name) (get (second x) :name)]))
+              (filter overlap? event-pairs)))))
 
 ; walk-intervals performs accumulative recursion through the sorted
 ; list of event components, collecting conflicts whenever a new start component
-; is seen before an event is closed off by it's end component.
+; is seen before an event is closed off by its end component.
 ;
-; components are created by first generating all possible pairs of event
-; names in the 'active' set, and mapping the make-conflict constructor on them.
-; these conflicts are added to the conflicts sequence, which is returned upon
-; running out of event components.
+; for every start component, new conflicts are potentially accumulated.
+; this is done by a set union of all pairs of newly 'active' events
+; (meaning their start has been seen, but not their end) and the previously
+; recorded conflicts. we use union here to ensure no duplicates are recorded.
+;
+; active events are stored in a set, and each time the end component of an
+; event is seen, the next recursive call uses the active set disjoint with
+; the recently ended event.
 (defn walk-intervals [components active conflicts]
   (cond
     (empty? components) conflicts
     (= :start (get (first components) :type))
      (let [next-active (conj active (get (first components) :name))
-           next-conflicts (conj conflicts (map make-conflict (combo/combinations next-active 2)))]
+           next-conflicts (set/union conflicts (set (combo/combinations next-active 2)))]
        (walk-intervals (rest components) next-active next-conflicts))
     (= :end (get (first components) :type))
      (let [next-active (disj active  (get (first components) :name))]
        (walk-intervals (rest components) next-active conflicts))))
 
 ; get-conflicts-intervals splits the events into their start and end
-; components, and sorts them. once sorted, it returns all events that have
-; starts within other events. looking for nested ends is redundant.
+; components, and sorts them. once sorted, it walks through the new list of
+; intervals, and records conflicts for all starts that occur before previous
+; events have ended.
 ;
 ; event components are sorted by timestamp, which is represented by the
-; unix timestamp (milliseconds since epoch).
+; unix timestamp (milliseconds since epoch), calculated in the Event constructor
 ;
-; this approach is must faster that the naive approach, and since it only
+; this approach is must faster than the naive approach, and since it only
 ; requires one scan of the events after sorting, the time complexity of
 ; this solution is O(nlogn)
 (defn get-conflicts-intervals [list-of-events]
   (let [components (map event-to-components list-of-events)
         sorted-components (sort-by (fn [x] (get x :timestamp)) (flatten components))]
-    (walk-intervals sorted-components (set []) [])))
+    (walk-intervals sorted-components #{} #{})))
--- test/double_booked/core_test.clj
+++ test/double_booked/core_test.clj
@@ -1,9 +1,52 @@
 (ns double-booked.core-test
   (:require [clojure.test :refer :all]
-            [double-booked.core :refer :all])
-  (:import  [double_booked.core Event]))
+            [double-booked.core :refer :all]))
 
+(def t1 (make-event "My Birthweek" "1995-05-31" "1995-06-07"))
+(def t2 (make-event "My Birthday" "1995-05-31" "1995-06-01"))
+(def t3 (make-event "First of June" "1995-06-01" "1995-06-02"))
+(def t4 (make-event "Winter Break" "1995-12-22" "1996-01-05"))
+(def t5 (make-event "Christmas" "1995-12-25" "1995-12-26"))
+(def t6 (make-event "Nest1" "2017-05-01" "2017-05-31"))
+(def t7 (make-event "Nest2" "2017-05-05" "2017-05-17"))
+(def t8 (make-event "Nest3" "2017-05-07" "2017-05-18"))
+(def t9 (make-event "Nest4" "2017-05-07" "2017-05-08"))
+(def t10 (make-event "Nest5" "2017-05-07T12:00:04" "2017-05-07T12:04:04"))
 
-(deftest is-birthday
-  (testing "Is my birthday entered correctly?"
-    (is (= a (Event. "My Birthday" "05-31-95T00:00:00" "05-31-95T23:59:59")))))
+; The results are converted to sets because both algorithms create the pairs
+; in different orders. Thus, to prove they have the same output, converting
+; all pairs to sets is necessary when checking for equality of event conflicts
+(defn pairs-to-sets [pairs] (set (map (fn [x] (set x)) pairs)))
+
+(def winter-break-overlap-sets #{#{"Winter Break" "Christmas"}})
+
+(def nested-overlap-sets #{#{"Nest1" "Nest4"}
+                           #{"Nest2" "Nest4"}
+                           #{"Nest1" "Nest2"}
+                           #{"Nest1" "Nest3"}
+                           #{"Nest4" "Nest5"}
+                           #{"Nest1" "Nest5"}
+                           #{"Nest3" "Nest4"}
+                           #{"Nest3" "Nest5"}
+                           #{"Nest2" "Nest5"}
+                           #{"Nest2" "Nest3"}})
+
+(deftest winter-break-overlap-naive
+  (testing "Winter break overlap with Christmas, naive algorithm"
+    (is (= winter-break-overlap-sets
+           (pairs-to-sets (get-conflicts-naive [t4 t5]))))))
+
+(deftest winter-break-overlap-intervals
+  (testing "Winter break overlap with Christmas, intervals algorithm"
+    (is (= winter-break-overlap-sets
+           (pairs-to-sets (get-conflicts-intervals [t4 t5]))))))
+
+(deftest nesting-events-naive
+  (testing "Five overlapping and increasingly shorter events, naive algorithm"
+    (is (= nested-overlap-sets
+           (pairs-to-sets (get-conflicts-naive [t6 t7 t8 t9 t10]))))))
+
+(deftest nesting-events-intervals
+  (testing "Five overlapping and increasingly shorter events, intervals algorithm"
+    (is (= nested-overlap-sets
+           (pairs-to-sets (get-conflicts-intervals [t6 t7 t8 t9 t10]))))))
